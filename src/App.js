ef54ffe8d1ad0842a3f85feb794db9024148ec7c
safanah13 <safanah11@safanah11.com>
2025-03-09 14:02:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>مرحباً بكم في نظام إدارة المدرسة</h1>
+      </header>
+      <main>
+        {/* هنا سيتم إضافة المكونات الرئيسية للتطبيق */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
 
