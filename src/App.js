13c62626a3f42439178c8ca43b8094dc7a2de571
safanah13 <safanah11@safanah11.com>
2025-03-09 14:39:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
-        <h1>مرحباً بكم في نظام إدارة المدرسة</h1>
+        <h1>مرحباً بكم في نظام الاستعاضة المدرسي</h1>
+        <p>
+          نظام إدارة الاستعاضة للمدارس
+        </p>
+        <button className="login-button">
+          تسجيل الدخول
+        </button>
       </header>
-      <main>
-        {/* هنا سيتم إضافة المكونات الرئيسية للتطبيق */}
-      </main>
     </div>
   );
 }
 
 export default App;
-
