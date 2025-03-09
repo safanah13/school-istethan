e45162f4d45a7dcf3e8edb5527162e2b0858941a
safanah13 <safanah11@safanah11.com>
2025-03-09 14:35:30 +0300

    Update navbar.js
    إضافة مكون شريط التنقل

--- src/navbar.js
+++ src/navbar.js
@@ -1,1 +1,27 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          <a href="/">نظام الاستعاضة المدرسي</a>
+        </div>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <a href="/" className="nav-link">الرئيسية</a>
+          </li>
+          <li className="nav-item">
+            <a href="/about" className="nav-link">من نحن</a>
+          </li>
+          <li className="nav-item">
+            <a href="/contact" className="nav-link">اتصل بنا</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
 
