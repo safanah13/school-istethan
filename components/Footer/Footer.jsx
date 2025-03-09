51bbaba287552138880cd4476300b64a4d0a09af
safanah13 <safanah11@safanah11.com>
2025-03-09 14:53:10 +0300

    Update Footer.jsx

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,1 +1,11 @@
+import styles from './Footer.module.css';
 
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>جميع الحقوق محفوظة © {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
