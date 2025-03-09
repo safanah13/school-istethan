5d02d69710a9775c350be06ef68b386d9749dabd
safanah13 <safanah11@safanah11.com>
2025-03-09 15:03:09 +0300

    Update Navbar.jsx

--- Navbar/Navbar.jsx
+++ Navbar/Navbar.jsx
@@ -1,1 +1,28 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          <a href="/">نظام الاستعاضة المدرسي</a>
+        </div>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <a href="/" className="nav-link">الرئيسية</a>
+          </li>
+          <li className="nav-item">
+            <a href="/about" className="nav-link">من نحن</a>
+          </li>
+          <li className="nav-item">
+            <a href="/contact" className="nav-link">اتصل بنا</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
 
